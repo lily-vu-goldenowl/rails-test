a7ab6ee702d554b97d6c77aefdb32f1e0c601915
@lily.vu.goldenowl <lily.vu.goldenowl@gmail.com>
2022-06-17 18:49:36 +0700

    Add ex 1

--- exercise_1/resolver.rb
+++ exercise_1/resolver.rb
@@ -1,1 +1,18 @@
-# write code here...
+def pair_of_two_distinct_elements(sum, arr)
+  result = []
+  filted_arr = arr.uniq.sort
+  length_arr = filted_arr.length()
+
+  filted_arr[0..(length_arr - 2)].each_with_index do |element, index|
+    filted_arr[(index + 1)..(length_arr - 1)].each_with_index do |element2, index2|
+      if element + element2 == sum
+        result << [element, element2]
+      end
+    end
+  end
+  return result
+end
+
+result = pair_of_two_distinct_elements(10, [1, 4, 2, 1, 3, 6, 5])
+
+puts result
